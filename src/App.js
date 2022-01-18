219d62fc9cf68a64512e165a16acb34625cab75a
Ramzan <ramzan4ik95@mail.ru>
2022-01-18 12:56:37 +0300

    new files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './components/HomePage';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/signin' />
+        <Route path='/signup'/>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
   );
 };
 
