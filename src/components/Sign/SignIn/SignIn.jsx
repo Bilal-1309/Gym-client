aac5ec468111a12117883585cd93560559dfecdc
Amir <amir.95mail@gmail.com>
2022-01-18 17:39:40 +0300

    вывод ошибок

--- src/components/Sign/SignIn/SignIn.jsx
+++ src/components/Sign/SignIn/SignIn.jsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../../redux/features/auth";
+
 export default function SignIn() {
-    return(
-        <div>
-            вход
-        </div>
-    )
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signingIn = useSelector((state) => state.auth.signingIn);
+  const error = useSelector((state) => state.auth.error);
+
+  const handleChangeLogin = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    dispatch(login(email, password));
+  };
+
+  return (
+    <div>
+      {error}
+      <div>
+        <input
+          type="email"
+          placeholder="type email"
+          value={email}
+          onChange={handleChangeLogin}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="type password"
+          value={password}
+          onChange={handleChangePassword}
+        />
+      </div>
+      <button onClick={handleSubmit} disabled={signingIn}>
+        авторизация
+      </button>
+    </div>
+  );
 }
