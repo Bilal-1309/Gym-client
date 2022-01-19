7d11b7029e64dbe1c233be85598399cb3d3be0a7
Bilal <obkh1309@mail.ru>
2022-01-19 14:56:09 +0300

    начало работы на компоненте

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import styles from './profile.module.css'
 
 const Profile = () => {
   return (
-    <div>
+    <div className={styles.profile}>
+      <div className={styles.header}>
+        <div className={styles.header__name}>Билал Орзамиев</div>
+        <div className={styles.header__purpose}>Быть лучше!</div>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.main__profile}></div>
+        <div className={styles.main__info}></div>
+      </div>
+      <div className={styles.footer}></div>
       
     </div>
   );
