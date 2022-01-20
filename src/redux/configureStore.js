97d4bb416e90312f5e27407517a0e390822d20fc
Iznaur18 <gimer8@mail.ru>
2022-01-20 15:05:40 +0300

    admin login reduce sank

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,13 +4,15 @@ import { createLogger } from "redux-logger/src";
 import auth from "./features/auth";
 import { trainerReducer } from "./features/trainer";
 import { subscriptionsReducer } from "./features/subscription";
+import { profileReducer } from "./features/profile";
+import { adminReducer } from "./features/admin";
 
 const logger = createLogger({
   diff: true,
   collapsed: true,
 });
 
-const combineReducer = combineReducers({ auth, subscriptionsReducer, trainerReducer });
+const combineReducer = combineReducers({ auth, subscriptionsReducer, trainerReducer, profileReducer, adminReducer});
 
 export const store = createStore(
   combineReducer,
