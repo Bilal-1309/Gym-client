9acd291d502c41ac0aba3c4c826f17c92f2d65c3
Iznaur18 <gimer8@mail.ru>
2022-01-20 10:38:40 +0300

    made gallery

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,10 +1,77 @@
 import React from "react";
-import styles from './gallery.module.css'
+import styles from "./gallery.module.css";
+import photo1 from "./photo1.png";
+import logo from "./logo.png";
 
 const index = () => {
   return (
     <div className={styles.gallery__container}>
-      <h1 className={styles.gallery__title}>Галерея</h1>
+      <div className={styles.gallery__block}>
+        <h1 className={styles.gallery__title}>Галерея</h1>
+        <div className={styles.gallery__row}>
+          <div className={styles.gallery__text}>
+            <div>
+              <div className={styles.gallery__line}></div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                iaculis aliquam at porttitor placerat. Mattis venenatis ac
+                bibendum viverra integer auctor pharetra ut vivamus. Sed a
+                faucibus lobortis sed morbi scelerisque sed sed. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Sed iaculis aliquam
+                at porttitor placerat. Mattis venenatis ac bibendum viverra
+                integer auctor pharetra ut vivamus. Sed a faucibus lobortis sed
+                morbi scelerisque sed sed.Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Sed iaculis aliquam at porttitor
+                placerat. Mattis venenatis ac bibendum viverra integer auctor
+                pharetra ut vivamus. Sed a faucibus lobortis sed morbi
+                scelerisque sed sed. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Sed iaculis aliquam at porttitor placerat.
+                Mattis venenatis ac bibendum viverra integer auctor pharetra ut
+                vivamus. Sed a faucibus lobortis sed morbi scelerisque sed sed.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                iaculis aliquam at porttitor placerat. Mattis venenatis ac
+                bibendum viverra integer auctor pharetra ut vivamus. Sed a
+                faucibus lobortis sed morbi scelerisque sed sed.Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Sed iaculis aliquam
+                at porttitor placerat. Mattis venenatis ac bibendum viverra
+                integer auctor pharetra ut vivamus. Sed a faucibus lobortis sed
+                morbi scelerisque sed sed.
+              </p>
+            </div>
+          </div>
+          <div className={styles.gallery__photo}>
+            <img id="gallery" src={photo1} alt="" />
+          </div>
+        </div>
+
+        <div className={styles.gallery__row2}>
+          <div className={styles.gallery__logo}>
+            <img src={logo} alt="" />
+          </div>
+          <div className={styles.gallery__logoinfo}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              iaculis aliquam at porttitor placerat. Mattis venenatis ac
+              bibendum viverra integer auctor pharetra ut vivamus. Sed a
+              faucibus lobortis sed morbi scelerisque sed sed.Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Sed iaculis aliquam at
+              porttitor placerat. Mattis venenatis ac bibendum viverra integer
+              auctor pharetra ut vivamus. Sed a faucibus lobortis sed morbi
+              scelerisque sed sed. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed iaculis aliquam at porttitor placerat. Mattis
+              venenatis ac bibendum viverra integer auctor pharetra ut vivamus.
+              Sed a faucibus lobortis sed morbi scelerisque sed sed. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Sed iaculis aliquam
+              at porttitor placerat. Mattis venenatis ac bibendum viverra
+              integer auctor pharetra ut vivamus. Sed a faucibus lobortis sed
+              morbi scelerisque sed sed.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed iaculis aliquam at porttitor placerat. Mattis
+              venenatis ac bibendum viverra integer auctor pharetra ut vivamus.
+              Sed a faucibus lobortis sed morbi scelerisque sed sed.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
