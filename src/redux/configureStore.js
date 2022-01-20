f1763e3ae32b69eb71299223637fb2ccd56bc3b9
Bilal <obkh1309@mail.ru>
2022-01-20 15:05:35 +0300

    Добавление возможности перецти на свою страницу для юзера, так и для админа

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,13 +4,14 @@ import { createLogger } from "redux-logger/src";
 import auth from "./features/auth";
 import { trainerReducer } from "./features/trainer";
 import { subscriptionsReducer } from "./features/subscription";
+import { profileReducer } from './features/profile';
 
 const logger = createLogger({
   diff: true,
   collapsed: true,
 });
 
-const combineReducer = combineReducers({ auth, subscriptionsReducer, trainerReducer });
+const combineReducer = combineReducers({ auth, subscriptionsReducer, trainerReducer, profileReducer });
 
 export const store = createStore(
   combineReducer,
