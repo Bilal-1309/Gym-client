529b3039fe11e87ef72c35769457579ad0f07ea7
Amir <amir.95mail@gmail.com>
2022-01-27 18:00:32 +0300

    сделал футер

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header/'
 import Subscriptions from './Subscriptions';
+import Footer from './Footer/'
+
 
 const HomePage = () => {
   return (
     <div>
       <Header />
       <Subscriptions />
+      <Footer />
     </div>
   );
 };
