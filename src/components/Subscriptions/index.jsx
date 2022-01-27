2b59840a3d3d1ea258052c2cd01689e342ac5530
adam <mutaev@mail.ru>
2022-01-27 16:17:37 +0300

    Изменение стилей

--- src/components/Subscriptions/index.jsx
+++ src/components/Subscriptions/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadSubscriptions } from "../../redux/features/subscription";
+import Carousel from 'react-elastic-carousel'
 import styles from "./subscription.module.css";
 import { loadUserSubscription, subscriptionAddInCart } from '../../redux/features/profile';
 import { loadCartItems } from '../../redux/features/cart';
@@ -23,7 +24,7 @@ const Subscriptions = () => {
     dispatch(loadSubscriptions());
   }, [dispatch]);
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(loadCartItems(profileId))
   }, [dispatch])
 
@@ -35,25 +36,35 @@ const Subscriptions = () => {
       <div className={styles.container}>
         <h1 className={styles.carts__title}>Абонементы</h1>
         <div className={styles.carts__items}>
-
+          <Carousel
+          itemsToShow={3} 
+          autoPlaySpeed={5000} 
+          enableAutoPlay
+          easing="cubic-bezier(1,.15,.55,1.54)"
+          tiltEasing="cubic-bezier(0.110, 1, 1.000, 0.210)"
+          transitionMs={700}
+        >
           {!load ? subscriptions.map((subscription) => {
             return (
-              <figure className={styles.cart} key={subscription._id}>
-                <h2 className={styles.cart__img__title}>{subscription.name}</h2>
-                <img src={`http://localhost:5000/${subscription.img}`} alt="" />
-                <figcaption>
-                  <h3 className={styles.cart__price}>{subscription.price} ₽</h3>
-                  <p>Абонемент на: {subscription.time /3600 / 24} дней</p>
-                  <p>{subscription.text}</p>
-                  <button onClick={()=> token ? handleAddSubscription( subscription._id): navigate('/signin')}>Купить</button>
-                </figcaption>
-              </figure>
-            );
+              <>
+                  <figure className={styles.cart} key={subscription._id}>
+                    <h2 className={styles.cart__img__title}>{subscription.name}</h2>
+                    <img src={`http://localhost:5000/${subscription.img}`} alt="" />
+                    <figcaption>
+                      <h3 className={styles.cart__price}>{subscription.price} ₽</h3>
+                      <p>Абонемент на: {subscription.time / 3600 / 24} дней</p>
+                      <p>{subscription.text}</p>
+                      <button onClick={() => token ? handleAddSubscription(subscription._id) : navigate('/signin')}>Купить</button>
+                    </figcaption>
+                  </figure>
+                </>
+                );
           }) : null}
-          
+
+              </Carousel>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
